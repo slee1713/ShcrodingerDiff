9cdbdd7a72a374a490e91866a4cadff529915b90
Spencer Lee <slee216@hawk.iit.edu>
2021-03-19 12:49:06 -0500

    Initial Commit

---
+++ Shared/ShcrodingerDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShcrodingerDiffApp.swift
+//  Shared
+//
+//  Created by Spencer Lee on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ShcrodingerDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
